43c4b793148b1c2357f57751ed98a55475c3deb1
vtolstikov <vtolstikov@skbkontur.ru>
2026-03-13 12:37:40 +0500

    remove comments

--- src/createDra.tsx
+++ src/createDra.tsx
@@ -17,7 +17,6 @@ export function createDra<TValue, TParams, TParentResult>(
   const refreshAtom = atom(0);
 
   let counter = 0;
-  // const oldDisposeRef = atom({ dispose: async () => {} });
   const lastMutableTcsRef = atom({cancel: async (_reason: object) => [] as void[]})
 
   const disposeRefreshAtom = atom(
@@ -25,7 +24,6 @@ export function createDra<TValue, TParams, TParentResult>(
       const _counter = counter++;
       console.log("run get", name, _counter);
       get(refresher);
-      // const ref = get(oldDisposeRef);
       const _syncParams = get(syncParams);
       const result = parentDra ? await get(parentDra) : undefined;
 
@@ -36,21 +34,9 @@ export function createDra<TValue, TParams, TParentResult>(
       get(lastMutableTcsRef).cancel = tcs.cancel
 
       return await create(_syncParams, tcs.token, result);
-
-      // const getNewRes = (async () => {
-      //   console.log("creating", name, _counter);
-      //   try {
-      //     await ref.dispose();
-      //   } catch (e) {}
-      // })();
-      // const resPromise = getNewRes.then((x) => x);
-      // console.log("adding dispose", name, _counter);
-      // ref.dispose = () => getNewRes.then((x) => x.dispose());
     },
     (get, set) => {
       console.log("cleanup");
-      // get(oldDisposeRef).dispose();
-      // token.cancel();
       get(lastMutableTcsRef).cancel({});
 
       set(refresher);
